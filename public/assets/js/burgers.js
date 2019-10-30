ea8f2c4b3774644d553dd579a39f36ad6ba286b3
Kyle <kbrooks353@gmail.com>
2019-10-29 23:22:39 -0700

    changed the necessary src links and connections to make it work

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,49 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEaten = $(this).data("neweaten");
+    console.log(id);
+    console.log(newEaten);
+
+    var newEatenState = {
+      eaten: newEaten
+    };
+    console.log(newEatenState);
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenState
+    }).then(
+      function() {
+        console.log("changed eaten to", newEaten);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#ca").val().trim()
+      // eaten: $("[name=eaten]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
