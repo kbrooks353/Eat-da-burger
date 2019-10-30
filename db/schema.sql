90b2fd1f728655272f87eac48663dcef85759da9
Kyle <kbrooks353@gmail.com>
2019-10-29 20:04:29 -0700

    copy pasted a lot of starter code

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL ATO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    eaten BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO burgers (name) VALUES ("DOUBLE-DOUBLE");
+INSERT INTO burgers (name) VALUES ("DOUBLE-DOUBLE ANIMAL STYLE");
+INSERT INTO burgers (name, eaten) VALUES ("FAMOUS STAR", true);
+INSERT INTO burgers (name, eaten) VALUES ("CHARBROILED BURGER", true);
